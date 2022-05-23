1c91709e72033d0c1414150e4823c2209caf8b0c
theNvN <thenvnsahu@gmail.com>
2022-05-23 14:47:14 +0530

    initial commit

--- src/lib.rs
+++ src/lib.rs
@@ -3,24 +3,9 @@ extern crate log;
 
 mod vm;
 
-#[allow(unused_imports)]
-#[allow(dead_code)]
-fn add_one(num: i32) -> i32 {
-    info!("add_one called with {}", num);
-    num + 1
-}
-
 #[cfg(test)]
 mod tests {
     use super::*;
-
-    #[test]
-    fn it_adds_one() {
-        // env_logger::init();
-
-        info!("can log from the test too");
-        assert_eq!(3, add_one(2));
-    }
 
     #[test]
     fn it_works() {
